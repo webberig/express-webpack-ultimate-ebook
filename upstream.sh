03ce0536e1936a1823b07c373adefb0b35e88764
Mathieu Maes <mathieu@zimmo.be>
2018-12-29 11:15:03 +0100

    Improve upstream

--- upstream.sh
+++ upstream.sh
@@ -1,5 +1,8 @@
 if !(git ls-remote --exit-code upstream) then
     git remote add git@github.com:webberig/webpack-express-ultimate-guide.git
 fi
+git checkout master && git pull
 git fetch upstream
 git merge upstream/master
+git push
+
